d325c93d2211d3bedaa89e40a99d59481b235a66
YmendezM <ysmendezm@gmail.com>
2021-06-11 22:06:09 -0400

    Frist commit

---
+++ src/main/java/com/ym/DemogusApplication.java
@@ -0,0 +1,13 @@
+package com.ym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemogusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemogusApplication.class, args);
+	}
+
+}
